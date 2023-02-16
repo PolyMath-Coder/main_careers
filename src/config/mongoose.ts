359c951cb3b680435d5b061d0a0c3b8005db5b3f
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-02-16 14:53:29 +0100

    ready to deploy

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,26 +1,13 @@
-import mongoose from 'mongoose';
+import { connect } from 'mongoose';
 import dotenv from 'dotenv';
-import { defaultFieldResolver } from 'graphql';
 dotenv.config();
+
 const DB_URL = process.env.DB_URL;
 
-const connectToDatabase = (): any => {
-  const DB_URL = process.env.DB_URL;
+const connectToDatabase = () => {
   const options = { autoIndex: false, maxPoolSize: 10 };
-  mongoose
-    .connect(
-      'mongodb+srv://Ayoluwa:%40Faithie123@cluster0.nm9b8.mongodb.net/main_careers'
-    )
-    .then((result) => {
-      console.log(`Database now Connected...`);
-    });
-  const database = mongoose.connection;
-  database.on('connected', async () => {
-    console.log('Database now Connected...');
-  });
-
-  mongoose.connection.on('error', (err): any => {
-    console.log('An error occurred while connecting to MongoDB', err);
+  connect(`${DB_URL}`).then(() => {
+    console.log(`Database now Connected...`);
   });
 };
 
